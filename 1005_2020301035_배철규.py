9d1be1e48efef42e0a8a9f8a89dbca089a2bac67
CGBae <145093722+CGBae@users.noreply.github.com>
2023-10-08 23:00:48 +0900

    Add files via upload

---
+++ 1005_2020301035_배철규.py
@@ -0,0 +1,449 @@
+Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301305_배철규code07-02.py
+1번째 숫자 : 10
+2번째 숫자 : 20
+3번째 숫자 : 30
+4번째 숫자 : 40
+Traceback (most recent call last):
+  File "C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301305_배철규code07-02.py", line 11, in <module>
+    print("합계 ==> $d" %hap)
+TypeError: not all arguments converted during string formatting
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301305_배철규code07-02.py
+1번째 숫자 : 10
+2번째 숫자 : 20
+3번째 숫자 : 30
+4번째 숫자 : 40
+합계 ==> 100
+aa = []
+
+aa.append(0)
+aa.append(0)
+aa.append(0)
+aa.append(0)
+print(aa)
+[0, 0, 0, 0]
+aa = []
+for i in range(0, 100) :
+    aa.append(0)
+len(aa)
+SyntaxError: invalid syntax
+len(aa)
+0
+aa = []
+for i in range(0, 100) :
+    aa.append(0)
+
+    
+len(aa)
+100
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-03.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-03.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-03.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-03.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 0
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 0
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 10
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 9, in <module>
+    aa[i] = (int(input(str(i + 1) + "번째 숫자 : ")))
+IndexError: list assignment index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 9, in <module>
+    aa[i] = (int(input(str(i + 1) + "번째 숫자 : ")))
+IndexError: list assignment index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 9, in <module>
+    aa[i] = int(input(str(i + 1) + "번째 숫자 : "))
+IndexError: list assignment index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 0
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 10
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 0
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 1
+3번째 숫자 : 1
+4번째 숫자 : 1
+5번째 숫자 : 1
+6번째 숫자 : 1
+7번째 숫자 : 1
+8번째 숫자 : 1
+9번째 숫자 : 1
+10번째 숫자 : 1
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 12, in <module>
+    hap = hap + aa[i]
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 14, in <module>
+    hap = hap + aa[i]
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 23, in <module>
+    hap = hap + aa[i]
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+합계 ==> 0
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py", line 25, in <module>
+    hap = hap + aa[i]
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+합계 ==> 45
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+합계 ==> 55
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규SELFSTUDY7-1.py
+1번째 숫자 : 1
+2번째 숫자 : 2
+3번째 숫자 : 3
+4번째 숫자 : 4
+5번째 숫자 : 5
+6번째 숫자 : 6
+7번째 숫자 : 7
+8번째 숫자 : 8
+9번째 숫자 : 9
+10번째 숫자 : 10
+합계 ==> 55
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py", line 12, in <module>
+    print("bb[0]에는 %d이, bb[99]에는 %d이 입력됩니다." % (bb[0], bb[99]))
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py", line 12, in <module>
+    print("bb[0]에는 %d이, bb[99]에는 %d이 입력됩니다." % (bb[0], bb[99]))
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py
+Traceback (most recent call last):
+  File "C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py", line 12, in <module>
+    print("bb[0]에는 %d이, bb[99]에는 %d이 입력됩니다." % (bb[0], bb[99]))
+IndexError: list index out of range
+
+= RESTART: C:\Users\skuniv\Documents\오픈소스프로그래밍_ㅂㅊㄱ\1005_2020301035_배철규code07-04.py
+bb[0]에는 198이, bb[99]에는 0이 입력됩니다.
+aa = [10, 20, 30, 40]
+aa[0:3]
+[10, 20, 30]
+aa[2:4]
+[30, 40]
+aa = [10, 20, 30]
+bb = [40, 50, 60]
+aa + bb
+[10, 20, 30, 40, 50, 60]
+aa * 3
+[10, 20, 30, 10, 20, 30, 10, 20, 30]
+>>> aa = [10, 20, 30, 40, 50, 60, 70]
+>>> aa[::2]
+[10, 30, 50, 70]
+>>> aa[::-2]
+[70, 50, 30, 10]
+>>> aa[::-1]
+[70, 60, 50, 40, 30, 20, 10]
+>>> aa = [10, 20, 30]
+>>> aa[1:2] = [200, 201]
+>>> aa
+[10, 200, 201, 30]
+>>> aa = [10, 20, 30]
+>>> del(aa[1])
+>>> aa
+[10, 30]
+>>> aa = [10, 20, 30, 40, 50]
+>>> aa[1:4] = []
+>>> aa
+[10, 50]
+>>> 
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-05.py
+현재 리스트: [30, 10, 20]
+append(40) 후의 리스트: [30, 10, 20, 40]
+pop()으로 추출한 값: <built-in method pop of list object at 0x000001D96BBB0180>
+pop() 후의 리스트: [30, 10, 20, 40]
+sort() 후의 리스트: [10, 20, 30, 40]
+reverse 후의 리스트: [40, 30, 20, 10]
+20값의 위치 : 2
+insert(2, 222) 후의 리스트: [40, 30, 222, 20, 10]
+remove(222) 후의 리스트: [40, 30, 20, 10]
+extend([77, 88, 77]) 후의 리스트: [40, 30, 20, 10, 77, 88, 77]
+77값의 개수 : 2
+>>> 
+= RESTART: C:/Users/skuniv/Documents/오픈소스프로그래밍_ㅂㅊㄱ/1005_2020301035_배철규code07-06.py
+  1  2  3  4
+  5  6  7  8
+  9 10 11 12
